f2b177a63a69a1cc25d23196cbe124d56a4acd1e
isaccanedo <isaccanedo@gmail.com>
2020-11-05 08:52:19 -0300

    Initial commit

---
+++ permutation.py
@@ -0,0 +1,18 @@
+def getPermutations(array):
+    if len(array) == 0:
+        return []
+    elif len(array) == 1:
+        return [array]
+    permutations = []
+
+    for i in range(len(array)):
+        array.insert(0, array.pop(i))
+        for perm in getPermutations(array[1:]):
+            perm.insert(0, array[0])
+            permutations.append(perm)
+
+    return permutations
+
+
+if __name__ == '__main__':
+    print(getPermutations([1, 2, 3, 4]))
